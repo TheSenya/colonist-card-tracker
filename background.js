4853b9e956f66ee66a33880a2960e36b106d1bd3
TheSenya <senyagithub@gmail.com>
2024-02-08 17:27:50 -0500

    updates

--- background.js
+++ background.js
@@ -1,16 +1,16 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("colonist.io/#")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
+// chrome.tabs.onUpdated.addListener((tabId, tab) => {
+//     if (tab.url && tab.url.includes("colonist.io/#")) {
+//       const queryParameters = tab.url.split("?")[1];
+//       const urlParameters = new URLSearchParams(queryParameters);
         
-      console.log(1, urlParameters, queryParameters)
+//       console.log(1, urlParameters, queryParameters)
   
-      chrome.tabs.sendMessage(tabId, {
-        type: "new_game",
-        value: urlParameters.get("v"),
-      });
-    }
-  });
+//       chrome.tabs.sendMessage(tabId, {
+//         type: "new_game",
+//         value: urlParameters.get("v"),
+//       });
+//     }
+//   });
 
 
   
--- script.js
+++ script.js
@@ -6,13 +6,19 @@
 // TheSenya discarded
 // Knight
 // received starting resources 
+// took from bank brick
+// took from bank brick wool
+// discarded 
+// Lesya stole  from you
 
 
 // names of images
 // card_lumber.svg
 // card_brick.svg
 // card_grain.svg
+// card_wool.svg
 // card_ore.svg
+
 // city_blue.svg
 // /dist/images/city_blue.svg?rev=0c1d112d674e204c57ad
 
@@ -25,21 +31,44 @@ let logCount = 0;
 // {'asd' : {'lumber': 0, 'brick': 0, 'sheep': 0, 'grain': 0, 'ore': 0}}
 let players = {}
 
-let phraseSet = Set([])
+let phraseSet = new Set(['stole  from', 'gave bank  and took', 'built a'])
+
+
+/**
+    * @param {string} type
+    * @param {Array} amountRange, specifies the range of amount of cards ie, [1,1] (1 card) or [1,2] 1-2 of that card
+    * @return {Promise<TicketingSystem.Ticket[]>}
+*/
+
+class Cards {
+    constructor(type, amountRange) {
+      this.type = type;
+      this.amountRange = amountRange;
+    }
+}
 
 setInterval(function () {
 
     var logs = document.querySelectorAll('.message-post')
     console.log(1, logs)
 
-    if(logs && logs.length > logCount){
+    // if a log exists and its a new log then read it 
+    if (logs && logs.length > logCount){
         console.log(2, logs.length)
 
         logs.forEach(function (message) {
             var text = message.innerText;
-            fullLogs = text
             console.log(text)
+
+
+            if (text) {
+                identifyAction(text, message)
+            }
+
+            
+            //console.log(text)
         });
+
     }
 
   }, 5000);
@@ -48,9 +77,46 @@ setInterval(function () {
 function identifyPlayers(){
     // we can do this by using the 'received starting resources ' text in the chat 
     // maybe also wise to come up with a usecase for when the players d/c and then reconnects
+
+    if (player in players) {
+        
+    }
+
 }
 
-function identifyAction(){
+function identifyAction(text, html){
+
+    if (text.includes('got')) {
+
+        let lumber = html.querySelectorAll('img[alt="lumber"]').length
+        let brick = html.querySelectorAll('img[alt="brick"]').length
+        let wool = html.querySelectorAll('img[alt="wool"]').length
+        let grain = html.querySelectorAll('img[alt="grain"]').length
+        let ore = html.querySelectorAll('img[alt="ore"]').length
+        // console.log('lumber = ', lumber)
+        // console.log('brick = ', brick)
+        // console.log('wool = ', wool)
+        // console.log('grain = ', grain)
+        // console.log('ore = ', ore)
+    }
+
+    // if (text.includes('got')) {
+        
+
+    // }else if (text.includes('gave bank  and took ')) {
+
+    // }else if (text.includes('stole  from')) {
+
+    // }else if (text.includes('got')) {
+
+    // }else if (text.includes('got')) {
+
+    // }else if (text.includes('got')) {
+
+    // }else if (text.includes('got')) {
+
+    // }
+
 
 }
 
@@ -65,6 +131,14 @@ function readLogs(){
         logs.forEach(function (message) {
             var text = message.innerText;
             console.log(text)
+
+
+            if (text) {
+                identifyAction(text, message)
+            }
+
+            
+            //console.log(text)
         });
 
     }
@@ -77,3 +151,14 @@ function readLogs(){
 // Need to compensate for the user reconnecting, when this happens the old chat will disapear and no info can be gained form it
 
 // 
+
+
+// setInterval(function () {
+
+//     var logs = document.querySelectorAll('.message-post')
+//     console.log(1, logs)
+    
+//     console.log('5sec')
+//     readLogs()
+
+//   }, 5000);
